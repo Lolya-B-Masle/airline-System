6e7d54bad86a8480e6390a7696b13a5191880f18
Vladislav Sharafiev <nyam6771@gmail.com>
2024-04-05 20:16:20 +0700

    Добавлена функция добавления и удаления рейса из таблицы

--- src/main/java/org/example/demo/Flight.java
+++ src/main/java/org/example/demo/Flight.java
@@ -4,16 +4,69 @@ public class Flight {
     int id;
     String name, path, start, end, startTime, endTime;
 
-    public Flight(String name, String path, String start, String end, String startTime, String endTime) {
+    public Flight(int id, String name, String path, String start, String end, String startTime, String endTime) {
         this.name = name;
         this.path = path;
         this.start = start;
         this.end = end;
         this.startTime = startTime;
         this.endTime = endTime;
-        id = name.length() - 1 + end.length();
+        this.id = id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
 }
--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -1,23 +1,32 @@
 package org.example.demo;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 
 public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private Button AddBtn;
 
     @FXML
-    private TableColumn<?, ?> Begin;
+    private TableColumn<Flight, String> Begin;
 
     @FXML
-    private TableColumn<?, ?> BeginTime;
+    private TableColumn<Flight, String> BeginTime;
 
     @FXML
     private TextField CountOfWrites;
@@ -29,28 +38,28 @@ public class HelloController {
     private Button EditBtn;
 
     @FXML
-    private TableColumn<?, ?> End;
+    private TableColumn<Flight, String> End;
 
     @FXML
-    private TableColumn<?, ?> EndTime;
+    private TableColumn<Flight, String> EndTime;
 
     @FXML
     private Button FilterBtn;
 
     @FXML
-    private TableColumn<?, ?> ID;
+    private TableColumn<Flight, Integer> ID;
 
     @FXML
     private MenuBar NavBar;
 
     @FXML
-    private TableColumn<?, ?> Path;
+    private TableColumn<Flight, String> Path;
 
     @FXML
     private Menu ReportTbElement;
 
     @FXML
-    private TableColumn<?, ?> Road;
+    private TableColumn<Flight, String> Road;
 
     @FXML
     private Button SearchBtn;
@@ -62,9 +71,129 @@ public class HelloController {
     private Menu ServiceTbElement;
 
     @FXML
-    private TableView<?> Table;
+    private TableView<Flight> Table;
 
     @FXML
     private Menu WindowsTbElement;
 
+    @FXML
+    private MenuItem canceledFlights;
+
+    @FXML
+    private MenuItem cashRegister;
+
+    @FXML
+    private MenuItem closeAll;
+
+    @FXML
+    private MenuItem completedFlights;
+
+    @FXML
+    private MenuItem flights;
+
+    @FXML
+    private MenuItem help;
+
+    @FXML
+    private MenuItem privileges;
+
+    @FXML
+    private MenuItem sendError;
+
+    @FXML
+    private MenuItem schedule;
+
+    ObservableList<Flight> list = FXCollections.observableArrayList(
+        new Flight(1, "ПЛ-524", "ВВО-ЕМВ-ПЛК", "26.06.24", "04.09.024", "6:05", "18:10"),
+        new Flight(2, "С7-107", "ДМД-ВВО", "01.06.24", "23.10.24", "14:00", "22:30"),
+        new Flight(3, "С7-108", "ВВО-ДМД", "01.06.24", "24.10.24", "1:00", "10:30"),
+        new Flight(4, "С7-3741", "ТЛЧ-ВВО-ПРЛ", "31.03.24", "23.10.24", "17:05", "3:05"),
+        new Flight(5, "С7-3272", "ПРЛ-ВВО-ТЛЧ", "01.04.24", "24.10.24", "4:55", "15:30"),
+        new Flight(6, "SU-719", "ШРМ-ВВО", "21.09.24", "18.10.24", "16:15", "0:45")
+    );
+
+    @FXML
+    void initialize() {
+        ID.setCellValueFactory(new PropertyValueFactory<Flight, Integer>("id"));
+        Road.setCellValueFactory(new PropertyValueFactory<Flight, String>("path"));
+        Path.setCellValueFactory(new PropertyValueFactory<Flight, String>("name"));
+        Begin.setCellValueFactory(new PropertyValueFactory<Flight, String>("start"));
+        End.setCellValueFactory(new PropertyValueFactory<Flight, String>("end"));
+        BeginTime.setCellValueFactory(new PropertyValueFactory<Flight, String>("startTime"));
+        EndTime.setCellValueFactory(new PropertyValueFactory<Flight, String>("endTime"));
+
+
+
+        Table.setItems(list);
+        int count = list.size();
+
+        CountOfWrites.setText("" + count);
+
+        AddBtn.setOnAction(event -> {
+            System.out.println("Запись добавлена");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+            alert.setTitle("Новый рейс");
+            alert.setHeaderText(null);
+            alert.setContentText("Новый тестовый рейс добавлен");
+
+            list.add(new Flight(list.size() + 1, "test", "test", "test", "test", "test", "test"));
+
+            int count1 = list.size();
+
+            CountOfWrites.setText("" + count1);
+
+            alert.showAndWait();
+        });
+
+        FilterBtn.setOnAction(event -> {
+            System.out.println("Применен фильтр");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+            alert.setTitle("Фильтр");
+            alert.setHeaderText(null);
+            alert.setContentText("Опции фильтра");
+
+            alert.showAndWait();
+        });
+
+        ServiceBtn.setOnAction(event -> {
+            System.out.println("Сервис выбран");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+            alert.setTitle("Сервис");
+            alert.setHeaderText(null);
+            alert.setContentText("Опции сервиса");
+
+            alert.showAndWait();
+        });
+
+        DelBtn.setOnAction(event -> {
+            System.out.println("Удаление рейса");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+            TableView.TableViewSelectionModel<Flight> selectionModel = Table.getSelectionModel();
+            System.out.println(selectionModel.getFocusedIndex());
+
+            list.remove(selectionModel.getFocusedIndex());
+
+            alert.setTitle("Удаление рейса");
+            alert.setHeaderText(null);
+            alert.setContentText("Рейс успешно удален");
+
+            alert.showAndWait();
+        });
+
+        EditBtn.setOnAction(event -> {
+            System.out.println("Выполнено редактирование рейса");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+            alert.setTitle("Редактировать рейс");
+            alert.setHeaderText(null);
+            alert.setContentText("Рейс редактирован");
+
+            alert.showAndWait();
+        });
+    }
 }
+
